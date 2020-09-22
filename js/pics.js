bf6013b6a77089c7049b758d43ee6cbcd9bb9f11
edwinmwiti <edwinmwiti658@gmail.com>
2020-09-22 14:15:20 +0300

    Change pictures with timeout function

---
+++ js/pics.js
@@ -0,0 +1,20 @@
+setTimeout(function name(params) {
+    // code to change pics
+    let pics = ['images/brush.jpg', 'images/mascara.jpg'];
+    let div = document.getElementById('home_div');
+
+    while (true) {
+        let image_index = 0;
+        // check if array end is reached
+        if(image_index === pics.length){
+            let counter = 0;
+            // change div background
+            div.style.backgroundImage = url(pics[image_index]);
+        }else{
+            div.style.backgroundImage = url(pics[image_index])
+        }
+
+        image_index++;
+    }
+
+}, 5000);
