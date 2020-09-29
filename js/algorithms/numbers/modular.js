8a431506f38368d3c1dec35dbb50cd926ca51284
edwinmwiti <emwiti658@gmail.com>
2020-09-29 15:16:29 +0300

    Modular exponentiation Algorithms

---
+++ js/algorithms/numbers/modular.js
@@ -0,0 +1,13 @@
+function modularExponentiation(base,exponent,modulus){
+    if(modulus === 1) return 0;
+
+    let value = 1;
+
+    for (let i =0; i<exponent; i++){
+        value = (value * base) % modulus;
+    }
+
+    return value;
+
+}
+
